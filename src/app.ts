a3be6dca41b4cd283f5408187d8ebff7df3504c4
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-12 20:32:20 -0300

    feat(app): initialize application

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,8 @@
-import { WhatsappProvider } from "./whatsappProvider";
 
-const whatsapp = new WhatsappProvider();
+import { CommandsUseCase } from "./aplications/usecases/commands";
+import { WhatsAppClient } from "./infrastructure/Whatsapp/WhatsappClient";
+
+const commands = new CommandsUseCase()
+const app = new WhatsAppClient(commands);
+
+app.initialize();

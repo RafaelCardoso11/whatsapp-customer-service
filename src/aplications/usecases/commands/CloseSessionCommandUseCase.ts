5728bbaad6215f9915c92ec6789b7a73c10f111a
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-14 22:37:51 -0300

    feat: CloseSessionCommand with reset repository consultant

--- src/aplications/usecases/commands/CloseSessionCommandUseCase.ts
+++ src/aplications/usecases/commands/CloseSessionCommandUseCase.ts
@@ -1,7 +1,45 @@
+import { Consultant } from "../../../infrastructure/database/entities/Consultant";
+import { AttendimentRepository } from "../../../infrastructure/database/repositories/Attendiment";
+import { ConsultantRepository } from "../../../infrastructure/database/repositories/Consultant";
 import { ICommand } from "./interfaces/command";
 
 export class CloseSessionCommand implements ICommand {
-  execute(idClient: string): string {
-    return `_Atendimento *#${1}* Encerrado com Sucesso_`;
+  async execute(consultant: Consultant): Promise<string> {
+    const { _id, name, number, clientCurrent } = consultant;
+    const consultantRepository = new ConsultantRepository();
+    const attendimentRepository = new AttendimentRepository();
+
+    const clientCurrentEmpty = {
+      _id: "",
+      name: "",
+      number: "",
+    };
+    if (clientCurrent._id) {
+      await consultantRepository.updateClientCurrent(_id, clientCurrentEmpty);
+
+      const attendimentCreated = await attendimentRepository.create({
+        attendimentStars: 5,
+        avaliation: [
+          {
+            content: "OK",
+          },
+        ],
+        consultant: {
+          name,
+          number,
+        },
+        date: new Date(),
+        client: clientCurrent,
+      });
+
+      if (attendimentCreated?.number) {
+        return `_Atendimento *#${attendimentCreated.number}* Encerrado com Sucesso_`;
+      }
+      return (
+        "Erro ao encerrar o Atendimento. Erro:" +
+        JSON.stringify(attendimentCreated)
+      );
+    }
+    return "Você não está em um atendimento!.";
   }
 }

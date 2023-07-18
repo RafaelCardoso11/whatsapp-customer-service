9eb99266f5fd858c2425bfe76ed8a58921f5e0cd
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-18 10:37:37 -0300

    feat: Add mongoDb configuration with process env

---
+++ src/infra/database/mongoDB.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+const config = {
+  username: process.env.MONGODB_USERNAME,
+  password: String(process.env.MONGODB_PASSWORD),
+  host: process.env.MONGODB_HOST,
+  port: process.env.MONGODB_PORT,
+  database: process.env.MONGODB_DATABASE,
+};
+
+const mongoURI = `mongodb://${config.host}:${config.port}/${config.database}`;
+
+export default mongoURI;

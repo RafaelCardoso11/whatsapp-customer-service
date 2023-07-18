7ad537da321358de2f5105e556c0abfe689eb175
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-18 13:04:24 -0300

    feat: adapter to logger

---
+++ src/adapters/LoggerAdapter.ts
@@ -0,0 +1,21 @@
+import { ILogger } from "../interfaces/logger";
+
+class LoggerAdapter implements ILogger {
+  private logger: ILogger;
+  constructor(logger: ILogger) {
+    this.logger = logger;
+  }
+  info(message: string, ...args: unknown[]) {
+    this.logger.info(message, args);
+  }
+  warn(message: string, ...args: unknown[]) {
+    this.logger.warn(message, args);
+  }
+  error(message: string, ...args: unknown[]) {
+    this.logger.error(message, args);
+  }
+  debug(message: string, ...args: unknown[]) {
+    this.logger.debug(message, args);
+  }
+}
+export { LoggerAdapter };

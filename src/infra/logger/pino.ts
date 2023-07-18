87e219ae53e2c3ef2ddd1ffa8ed8ff553a97feb3
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-18 13:06:04 -0300

    feat: create infra pino and configs

---
+++ src/infra/logger/pino.ts
@@ -0,0 +1,13 @@
+import pino from "pino";
+
+const PinoLogger = pino({
+  level: process.env.LOG_LEVEL || "info",
+  transport: {
+    target: "pino-pretty",
+    options: {
+      colorize: true,
+    },
+  },
+});
+
+export default PinoLogger;

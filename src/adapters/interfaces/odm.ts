be98ae85c74dec59a6552c0de52fc27d60c77997
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-19 15:58:57 -0300

    feat: interface odm

---
+++ src/adapters/interfaces/odm.ts
@@ -0,0 +1,10 @@
+export interface IOdm<SchemaType, ModelType> {
+  connection<Options extends Record<any, any>>(
+    uri: string,
+    options?: Options
+  ): Promise<void>;
+  createSchema<SchemaDefinition extends Record<any, any>, SchemaReturn>(
+    schemaDefinition: SchemaDefinition
+  ): SchemaReturn;
+  createModel(modelName: string, schema: SchemaType): ModelType;
+}

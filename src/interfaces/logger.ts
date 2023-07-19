07688f5c0c05f95d5bd670478abe3da6922224d2
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-18 22:10:35 -0300

    refactor: move paste interfaces to adapters/interfaces

--- src/interfaces/logger.ts
+++
@@ -1,6 +0,0 @@
-export interface ILogger {
-  info(message: string | unknown, ...args: unknown[]): void;
-  warn(message: string | unknown, ...args: unknown[]): void;
-  error(message: string | unknown, ...args: unknown[]): void;
-  debug(message: string | unknown, ...args: unknown[]): void;
-}

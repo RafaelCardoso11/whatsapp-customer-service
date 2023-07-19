6fc4246b6ea46f64639eb29b758adc3eac673110
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-19 15:59:23 -0300

    feat: odm implements interface odm with mongo adapter

---
+++ src/infra/odm/odm.ts
@@ -0,0 +1,32 @@
+import { Schema, SchemaDefinition, ConnectOptions, Model } from "mongoose";
+import { IOdm } from "../../adapters/interfaces/odm";
+import mongooseAdapter from "../../adapters/MongooseAdapter";
+
+class Odm<TSchema> implements IOdm<Schema, Model<any>> {
+  private odm: IOdm<Schema, Model<any>>;
+  Schema: TSchema;
+  constructor(Odm: IOdm<Schema, Model<any>>, schema: TSchema) {
+    this.odm = Odm;
+    this.Schema = schema;
+  }
+  async connection<Options extends ConnectOptions>(
+    uri: string,
+    options?: Options
+  ): Promise<void> {
+    await this.odm.connection(uri, options);
+  }
+  createSchema<
+    SchemaType extends SchemaDefinition,
+    SchemaReturn = typeof Schema
+  >(args: SchemaType): SchemaReturn {
+    return this.odm.createSchema(args);
+  }
+  createModel<SchemaType extends Schema>(
+    name: string,
+    schema?: any
+  ): Model<SchemaType | any> {
+    return this.odm.createModel(name, schema);
+  }
+}
+
+export default new Odm<typeof Schema>(new mongooseAdapter(), Schema);

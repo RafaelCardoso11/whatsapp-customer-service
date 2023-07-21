1ed61df4fad7eadd9fa449f451ec6ecbde04054a
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-20 22:36:55 -0300

    feat: create findConsultantWithoutClient, findConsultantByIdClient and updateClientCurrent

--- src/infra/repositories/Consultant.ts
+++ src/infra/repositories/Consultant.ts
@@ -1,6 +1,7 @@
-import { ConsultantModel } from "../../core/schemas/ConsultantSchema";
-import { Consultant } from "../../core/entities/Consultant";
-import { logger } from "../logger/logger";
+import { ConsultantModel } from '../../core/schemas/ConsultantSchema';
+import { Consultant } from '../../core/entities/Consultant';
+import { logger } from '../logger/logger';
+import { Client } from '../../core/entities/Client';
 
 class ConsultantRepository {
   async getById(id: string): Promise<Consultant | null> {
@@ -11,14 +12,15 @@ class ConsultantRepository {
     const consultants = await ConsultantModel.find().exec();
     return consultants.map((consultant) => consultant.toObject() as Consultant);
   }
-  async updateClientCurrent(
-    idConsultant: string,
-    clientCurrent: {
-      _id: string;
-      name: string;
-      number: string;
-    }
-  ): Promise<Consultant | null> {
+  async findConsultantWithoutClient(): Promise<Consultant | null> {
+    const consultantsWithoutClient = await ConsultantModel.findOne({ clientCurrent: { $exists: false } }).exec();
+    return consultantsWithoutClient ? (consultantsWithoutClient.toObject() as Consultant) : null;
+  }
+  async findConsultantByIdClient(idClient: string): Promise<Consultant | null> {
+    const consultantsWithoutClient = await ConsultantModel.findOne({ clientCurrent: { _id: idClient } }).exec();
+    return consultantsWithoutClient ? (consultantsWithoutClient.toObject() as Consultant) : null;
+  }
+  async updateClientCurrent(idConsultant: string, clientCurrent: Client): Promise<Consultant | null> {
     const consultantUpdated = await ConsultantModel.findOneAndUpdate(
       { _id: idConsultant },
       {
@@ -28,41 +30,28 @@ class ConsultantRepository {
       }
     ).exec();
 
-    return consultantUpdated
-      ? (consultantUpdated.toObject() as Consultant)
-      : null;
+    return consultantUpdated ? (consultantUpdated.toObject() as Consultant) : null;
   }
-  async updateConsultant(
-    idConsultant: string,
-    clientCurrent: { _id: string; name: string; number: string }
-  ) {
+  async updateConsultant(idConsultant: string, clientCurrent: Client) {
     const consultants = await this.getAll();
-    const consultantWithClientEmpty = consultants.find(
-      ({ clientCurrent: { _id } }) => _id === ""
-    );
+    const consultantWithClientEmpty = consultants.find(({ clientCurrent: { _id } }) => _id === '');
 
     if (consultantWithClientEmpty) {
-      const clientCurrentEmpty = {
-        _id: "",
-        name: "",
-        number: "",
+      const clientCurrentEmpty: Client = {
+        name: '',
+        telephone: '',
       };
       this.updateClientCurrent(idConsultant, clientCurrentEmpty);
       const newConsultant = await this.getById(consultantWithClientEmpty?._id);
       this.updateClientCurrent(idConsultant, clientCurrent);
       return newConsultant;
     } else {
-      logger.info("Nenhum consultor disponível");
+      logger.info('Nenhum consultor disponível');
     }
     return consultantWithClientEmpty;
   }
-  async create(consultant: {
-    name: string;
-    number: string;
-  }): Promise<Consultant | null> {
-    const created = (
-      await ConsultantModel.create(consultant)
-    ).toJSON() as Consultant;
+  async create(consultant: { name: string; number: string }): Promise<Consultant | null> {
+    const created = (await ConsultantModel.create(consultant)).toJSON() as Consultant;
 
     if (created) {
       return created;

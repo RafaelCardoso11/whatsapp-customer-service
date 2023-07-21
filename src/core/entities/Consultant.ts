01411ee5bcb889147aaf5b53ff8c2c349018e69b
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-21 17:26:57 -0300

    fix: use entity Client in entitity Consultant

--- src/core/entities/Consultant.ts
+++ src/core/entities/Consultant.ts
@@ -1,19 +1,12 @@
+import { Client } from './Client'
+
 class Consultant {
   _id: string
   name: string
   telephone: string
-  clientCurrent: {
-    _id: string
-    name: string
-    telephone: string
-  }
+  clientCurrent?: Client
 
-  constructor(
-    id: string,
-    name: string,
-    telephone: string,
-    clientCurrent: { _id: string; name: string; telephone: string }
-  ) {
+  constructor(id: string, name: string, telephone: string, clientCurrent: Client) {
     this._id = id
     this.name = name
     this.telephone = telephone

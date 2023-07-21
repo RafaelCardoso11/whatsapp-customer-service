9e1e7c8ad08078c6d8d974037a5ab9fd8e3d06f7
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-20 22:37:42 -0300

    refactor: lint file

--- src/core/usecases/commands/CloseSessionCommandUseCase.ts
+++ src/core/usecases/commands/CloseSessionCommandUseCase.ts
@@ -1,19 +1,17 @@
-
-import { AttendimentRepository } from "../../../infra/repositories/Attendiment";
-import { ConsultantRepository } from "../../../infra/repositories/Consultant";
-import { Consultant } from "../../entities/Consultant";
-import { ICommand } from "./interfaces/command";
+import { AttendimentRepository } from '../../../infra/repositories/Attendiment';
+import { ConsultantRepository } from '../../../infra/repositories/Consultant';
+import { Consultant } from '../../entities/Consultant';
+import { ICommand } from './interfaces/command';
 
 export class CloseSessionCommand implements ICommand {
   async execute(consultant: Consultant): Promise<string> {
-    const { _id, name, number, clientCurrent } = consultant;
+    const { _id, clientCurrent } = consultant;
     const consultantRepository = new ConsultantRepository();
     const attendimentRepository = new AttendimentRepository();
 
     const clientCurrentEmpty = {
-      _id: "",
-      name: "",
-      number: "",
+      name: '',
+      telephone: '',
     };
     if (clientCurrent._id) {
       await consultantRepository.updateClientCurrent(_id, clientCurrentEmpty);
@@ -22,13 +20,10 @@ export class CloseSessionCommand implements ICommand {
         attendimentStars: 5,
         avaliation: [
           {
-            content: "OK",
+            content: 'OK',
           },
         ],
-        consultant: {
-          name,
-          number,
-        },
+        consultant: consultant,
         date: new Date(),
         client: clientCurrent,
       });
@@ -36,11 +31,8 @@ export class CloseSessionCommand implements ICommand {
       if (attendimentCreated?.number) {
         return `_Atendimento *#${attendimentCreated.number}* Encerrado com Sucesso_`;
       }
-      return (
-        "Erro ao encerrar o Atendimento. Erro:" +
-        JSON.stringify(attendimentCreated)
-      );
+      return 'Erro ao encerrar o Atendimento. Erro:' + JSON.stringify(attendimentCreated);
     }
-    return "Você não está em um atendimento!.";
+    return 'Você não está em um atendimento!.';
   }
 }

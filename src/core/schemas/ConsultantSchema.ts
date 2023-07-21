f08d8427e388144161c5826b2ac63e59b1044e5c
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-20 22:34:11 -0300

    feat: _id client now is auto increment in db

--- src/core/schemas/ConsultantSchema.ts
+++ src/core/schemas/ConsultantSchema.ts
@@ -1,19 +1,19 @@
-import Odm from "../../infra/odm/odm";
+import Odm from '../../infra/odm/odm';
 
 const ConsultantSchema = Odm.createSchema({
   _id: { type: Odm.Schema.Types.ObjectId, auto: true },
   name: {
-    type: "string",
+    type: 'string',
     required: true,
   },
   number: {
-    type: "string",
+    type: 'string',
     required: true,
   },
   clientCurrent: {
-    _id: { type: "string", required: false, default: "" },
-    name: { type: "string", required: false, default: "" },
-    number: { type: "string", required: false, default: "" },
+    _id: { type: Odm.Schema.Types.ObjectId, auto: true },
+    name: { type: 'string', required: false, default: '' },
+    number: { type: 'string', required: false, default: '' },
   },
 });
-export const ConsultantModel = Odm.createModel("Consultants", ConsultantSchema);
+export const ConsultantModel = Odm.createModel('Consultants', ConsultantSchema);

4a63a02682a27e8bfec18d2e7e864a817dab1343
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-20 22:35:43 -0300

    feat: create sender class for sender messages. Implements IWhatsappSender

--- src/infra/Whatsapp/Sender.ts
+++ src/infra/Whatsapp/Sender.ts
@@ -1,9 +1,42 @@
-import { Whatsapp } from "venom-bot";
+import { IWhatsappSender } from '../../adapters/interfaces/whatsappSender'
+import { EMessageType } from '../../core/entities/Message'
 
-export class Sender {
-  constructor(private readonly client: Whatsapp) {}
+export class Sender implements IWhatsappSender {
+  private sender: IWhatsappSender
+  constructor(sender: IWhatsappSender) {
+    this.sender = sender
+  }
 
-  sendText(to: string, message: string): void {
-    this.client.sendText(to, message);
+  async execute(type: string, to: string, content: string): Promise<unknown> {
+    switch (type) {
+      case EMessageType.text:
+        return await this.sender.sendText(to, content)
+      case EMessageType.image:
+        return await this.sender.sendImage(to, content)
+      case EMessageType.voice:
+        return await this.sender.sendVoice(to, content)
+      case EMessageType.sticker:
+        return await this.sender.sendSticker(to, content)
+      default:
+        throw new Error('The message type is not supported')
+    }
+  }
+  async sendImage(to: string, content: string): Promise<object> {
+    return await this.sender.sendImage(to, content)
+  }
+  async sendVoice(to: string, content: string): Promise<unknown> {
+    return await this.sender.sendImage(to, content)
+  }
+  async sendSticker(to: string, content: string): Promise<false | object> {
+    return await this.sender.sendImage(to, content)
+  }
+  async sendVideoAsGif(to: string, content: string, filename: string, caption: string): Promise<void> {
+    await this.sender.sendVideoAsGif(to, content, filename, caption)
+  }
+  async sendDocument(to: string, content: string): Promise<unknown> {
+    return await this.sender.sendDocument(to, content)
+  }
+  async sendText(to: string, content: string): Promise<object> {
+    return await this.sender.sendImage(to, content)
   }
 }

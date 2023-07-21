af654c08c3d74e04a30a2e4e0af927d32b9953d0
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-20 22:33:03 -0300

    feat: entity message

---
+++ src/core/entities/Message.ts
@@ -0,0 +1,53 @@
+export enum EMessageType {
+  voice = "ptt",
+  text = "chat",
+  image = "image",
+  sticker = "sticker",
+}
+
+interface MediaData {
+  type: "image";
+  mediaStage: "FETCHING";
+  size: number;
+  loadedSize: number;
+  filehash: string;
+  mimetype: string;
+  mediaBlob: null;
+  fullHeight: number;
+  fullWidth: number;
+  aspectRatio: number;
+  animationDuration: number;
+  animatedAsNewMsg: boolean;
+  isViewOnce: boolean;
+  staticUrl: string;
+  preview: {
+    _retainCount: number;
+    _inAutoreleasePool: boolean;
+    released: boolean;
+    _b64: string;
+    _mimetype: string;
+  };
+  _swStreamingSupported: boolean;
+  _listeningToSwSupport: boolean;
+  isVcardOverMmsDocument: boolean;
+  waveform: null;
+}
+
+export interface Sender {
+  id: string;
+  name: string;
+  shortName: string;
+  pushname: string;
+}
+export interface IMessage {
+  id: string;
+  type: string;
+  from: string;
+  to: string;
+  isNewMsg: string;
+  hasReaction: string;
+  chatId: string;
+  sender: Sender;
+  mediaData: MediaData;
+  content: string;
+}

8d178953a5f175bfb4712b930fcc7625c4f1ddd6
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-22 16:58:52 -0300

    fix: type IWhatsappSender to unknown

--- src/adapters/interfaces/whatsappClient.ts
+++ src/adapters/interfaces/whatsappClient.ts
@@ -2,6 +2,6 @@ import { IMessage } from '../../core/entities/Message'
 import { IWhatsappSender } from './whatsappSender'
 
 export interface IWhatsappClient extends Partial<IWhatsappSender> {
-  initialize(session: string): Promise<IWhatsappSender>
+  initialize(session: string): Promise<unknown>
   onMessage(callback: (message: IMessage) => void): Promise<void>
 }

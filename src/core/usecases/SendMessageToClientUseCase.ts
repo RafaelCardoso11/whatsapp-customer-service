6e9806a2be20cab5e7f7afb36e0e93e8a9e11415
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-22 17:01:57 -0300

    feat: create SendMessageToClient

--- src/core/usecases/SendMessageToClientUseCase.ts
+++ src/core/usecases/SendMessageToClientUseCase.ts
@@ -1,13 +1,34 @@
+import constants from '../../constants'
 import { Sender } from '../../infra/Whatsapp/Sender'
-import { Client } from '../entities/Client'
-import { Consultant } from '../entities/Consultant'
+import { EMessageType } from '../entities/Message'
 
 export class SendMessageToClient {
   constructor(private readonly sender: Sender) {}
 
-  execute(messageType: string, client: Client, message: string) {
-    return this.sender.execute(messageType, client.telephone, message)
+  execute(messageType: string, clientTelephone: string, message: string): Promise<unknown> {
+    return this.sender.execute(messageType, clientTelephone, message)
   }
-  newAttendiment(consultant: Consultant, message: string) {}
-  messageFormattedWithNameConsultant(consultant: Consultant, message: string) {}
+  async newAttendiment(clientTelephone: string): Promise<boolean> {
+    const { MESSAGE_WAIT_FOR_CONSULTANT_1, MESSAGE_WAIT_FOR_CONSULTANT_2 } = constants.sucess
+
+    await this.sender.execute(EMessageType.TEXT, clientTelephone, MESSAGE_WAIT_FOR_CONSULTANT_1)
+    await this.sender.execute(EMessageType.TEXT, clientTelephone, MESSAGE_WAIT_FOR_CONSULTANT_2)
+
+    return true
+  }
+  messageFormattedWithNameConsultant(
+    clientTelephone: string,
+    consultantName: string,
+    messageType: string,
+    messageContent: string
+  ): Promise<unknown> {
+    const { MESSAGE_WITH_NAME_CONSULTANT_AND_CONTENT } = constants.sucess
+
+    const formattedMessage = MESSAGE_WITH_NAME_CONSULTANT_AND_CONTENT.replace(
+      '{consultantName}',
+      consultantName
+    ).replace('{messageContent}', messageContent)
+
+    return this.sender.execute(messageType, clientTelephone, formattedMessage)
+  }
 }

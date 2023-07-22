9716ee1ecdd4c960251c893dd25673e4c7e654d8
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-22 17:00:47 -0300

    feat: client init

--- src/infra/Whatsapp/Client.ts
+++ src/infra/Whatsapp/Client.ts
@@ -1,23 +1,23 @@
 import { IMessage } from '../../core/entities/Message'
 import { logger } from '../logger/logger'
 import { IWhatsappClient } from '../../adapters/interfaces/whatsappClient'
-import { IWhatsappSender } from '../../adapters/interfaces/whatsappSender'
+import { CommandsUseCase } from '../../core/usecases/commands'
 
 class WhatsappClient implements IWhatsappClient {
-  constructor(private readonly whatsappClient: IWhatsappClient) {}
+  constructor(private readonly whatsappClientAdapter: IWhatsappClient) {}
 
-  async initialize(): Promise<IWhatsappSender> {
+  async initialize(): Promise<IWhatsappClient> {
     try {
-      this.onMessage()
-      return await this.whatsappClient.initialize(process.env.SERVER_SESSION_WS as string)
+      return (await this.whatsappClientAdapter.initialize(process.env.SERVER_SESSION_WS as string)) as IWhatsappClient
     } catch (error) {
       logger.error('Error during application bootstrap', error)
       throw new Error(JSON.stringify(error))
     }
   }
   async onMessage(): Promise<void> {
-    await this.whatsappClient.onMessage((message: IMessage) => {
-      const { type, to, content } = message
+    await this.whatsappClientAdapter.onMessage((message: IMessage) => {
+      const { type, to, content } = message;
+      console.log(message)
     })
   }
 }

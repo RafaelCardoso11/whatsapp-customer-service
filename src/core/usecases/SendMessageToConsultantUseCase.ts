ab87068a9145dd1b6608999901ba0bf8d42d3641
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-22 17:01:47 -0300

    feat: create SendMessageToConsultant

---
+++ src/core/usecases/SendMessageToConsultantUseCase.ts
@@ -0,0 +1,17 @@
+import { formatterMessageClientWithInfoClient } from '../../helpers/formatterMessageClientWithInfosClient'
+import { Sender } from '../../infra/Whatsapp/Sender'
+import { Client } from '../entities/Client'
+import { EMessageType } from '../entities/Message'
+
+export class SendMessageToConsultant {
+  constructor(private readonly sender: Sender) {}
+
+  execute(messageType: string, consultantTelephone: string, message: string): Promise<unknown> {
+    return this.sender.execute(messageType, consultantTelephone, message)
+  }
+  messageFormattedWithInfosClient(client: Client, messageContent: string): Promise<unknown> {
+    const formattedMessage = formatterMessageClientWithInfoClient(client, messageContent)
+
+    return this.sender.execute(EMessageType.TEXT, client.telephone, formattedMessage)
+  }
+}

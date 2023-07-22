ecaba8306e7752809fd4a0b7d938443f8f88fbba
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-22 17:01:00 -0300

    feat: create formatterMessageClientWithInfoClient

---
+++ src/helpers/formatterMessageClientWithInfosClient.ts
@@ -0,0 +1,13 @@
+import constants from '../constants'
+import { Client } from '../core/entities/Client'
+
+export const formatterMessageClientWithInfoClient = (client: Client, message: string): string => {
+  const dateCurrent = new Date().toLocaleString('pt-BR')
+
+  const { MESSAGE_WITH_INFO_CLIENT } = constants.sucess
+
+  return MESSAGE_WITH_INFO_CLIENT.replace('{nameSaveClient}', String(client.nameSave))
+    .replace('{nameClient}', client.name)
+    .replace('{dateCurrent}', dateCurrent)
+    .replace('{messageContent}', message)
+}

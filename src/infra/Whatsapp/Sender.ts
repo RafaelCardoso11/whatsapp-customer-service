7d35b224e6cf60c018eb61825f5edba01d3fe76a
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-22 17:00:29 -0300

    feat: sender use senderAdapter to exec

--- src/infra/Whatsapp/Sender.ts
+++ src/infra/Whatsapp/Sender.ts
@@ -2,39 +2,38 @@ import { IWhatsappSender } from '../../adapters/interfaces/whatsappSender'
 import { EMessageType } from '../../core/entities/Message'
 
 export class Sender implements IWhatsappSender {
-  constructor(private sender: IWhatsappSender) {}
+  constructor(private senderAdapter: IWhatsappSender) {}
 
   async execute(type: string, to: string, content: string): Promise<unknown> {
     switch (type) {
       case EMessageType.TEXT:
-        console.log(this.sender, 'aa')
-        return await this.sender.sendText(to, content)
+        return await this.sendText(to, content)
       case EMessageType.IMAGE:
-        return await this.sender.sendImage(to, content)
+        return await this.sendImage(to, content)
       case EMessageType.VOICE:
-        return await this.sender.sendVoice(to, content)
+        return await this.sendVoice(to, content)
       case EMessageType.STICKER:
-        return await this.sender.sendSticker(to, content)
+        return await this.sendSticker(to, content)
       default:
         throw new Error('The message type is not supported')
     }
   }
   async sendImage(to: string, content: string): Promise<object> {
-    return await this.sender.sendImage(to, content)
+    return await this.senderAdapter.sendImage(to, content)
   }
   async sendVoice(to: string, content: string): Promise<unknown> {
-    return await this.sender.sendImage(to, content)
+    return await this.senderAdapter.sendVoice(to, content)
   }
   async sendSticker(to: string, content: string): Promise<false | object> {
-    return await this.sender.sendImage(to, content)
+    return await this.senderAdapter.sendSticker(to, content)
   }
   async sendVideoAsGif(to: string, content: string, filename: string, caption: string): Promise<void> {
-    await this.sender.sendVideoAsGif(to, content, filename, caption)
+    await this.senderAdapter.sendVideoAsGif(to, content, filename, caption)
   }
   async sendDocument(to: string, content: string): Promise<unknown> {
-    return await this.sender.sendDocument(to, content)
+    return await this.senderAdapter.sendDocument(to, content)
   }
   async sendText(to: string, content: string): Promise<object> {
-    return await this.sender.sendText(to, content)
+    return await this.senderAdapter.sendText(to, content)
   }
 }

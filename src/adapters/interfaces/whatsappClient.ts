f2e0b59b15dc693f35f5f41569267c7e6e72ba12
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-24 09:45:26 -0300

    feat: client extends sender

--- src/adapters/interfaces/whatsappClient.ts
+++ src/adapters/interfaces/whatsappClient.ts
@@ -1,7 +1,7 @@
 import { IMessage } from '../../core/entities/Message'
 import { IWhatsappSender } from './whatsappSender'
 
-export interface IWhatsappClient extends Partial<IWhatsappSender> {
-  initialize(session: string): Promise<unknown>
+export interface IWhatsappClient extends IWhatsappSender {
+  initialize(session: string): Promise<void>
   onMessage(callback: (message: IMessage) => void): Promise<void>
 }

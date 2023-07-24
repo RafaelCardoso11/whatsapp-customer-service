96dfe0110e78e582281a84dc661ca9495f8d24ba
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-24 09:46:45 -0300

    feat: list command with sender message list

--- src/core/usecases/commands/ListCommandsUseCase.ts
+++ src/core/usecases/commands/ListCommandsUseCase.ts
@@ -1,22 +1,19 @@
-import { CommandsWithDescription } from "./commands";
-import { ICommand } from "./interfaces/command";
+import { Sender } from '../../../infra/Whatsapp/Sender'
+import { Consultant } from '../../entities/Consultant'
+import { CommandsWithDescription } from './commands'
+import { ICommand } from './interfaces/command'
 
 export class ListCommandsCommand implements ICommand {
-  execute(): string {
-    return this.listCommands();
+  constructor(private readonly sender: Sender) {}
+
+  async execute(consultant: Consultant): Promise<void> {
+    await this.sender.sendText(consultant.telephone, this.listCommands())
   }
 
   private listCommands(): string {
     const commandList = Object.keys(CommandsWithDescription)
-      .map(
-        (command) =>
-          `*${command}* - ${
-            CommandsWithDescription[
-              command as keyof typeof CommandsWithDescription
-            ]
-          }`
-      )
-      .join("\n\n");
-    return commandList;
+      .map((command) => `*${command}* - ${CommandsWithDescription[command as keyof typeof CommandsWithDescription]}`)
+      .join('\n\n')
+    return commandList
   }
 }

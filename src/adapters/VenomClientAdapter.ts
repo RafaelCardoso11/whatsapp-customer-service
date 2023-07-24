971552c3591337a3302aaf9a1eb21de47c7e690f
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-24 09:38:46 -0300

    feat: now the adapter is responsability for telephone US

--- src/adapters/VenomClientAdapter.ts
+++ src/adapters/VenomClientAdapter.ts
@@ -1,27 +1,61 @@
-import { create, CreateOptions, Whatsapp } from 'venom-bot'
+import { create, CreateOptions, Message, SendStickerResult, Whatsapp } from 'venom-bot'
 
 import { IMessage } from '../core/entities/Message'
 
 import { IWhatsappClient } from './interfaces/whatsappClient'
+import { logger } from '../infra/logger/logger'
 
 class VenomClientAdapter implements IWhatsappClient {
   private whatsappClient: Whatsapp
+  private readonly US = '@c.us'
 
-  async initialize(session: string, ...rest: CreateOptions[]): Promise<Whatsapp> {
-    const whatsappClient = await create({ session, ...rest })
-    this.whatsappClient = whatsappClient
-    return whatsappClient
+  async initialize(session: string, ...rest: CreateOptions[]): Promise<void> {
+    try {
+      this.whatsappClient = await create({ session, ...rest })
+    } catch (error) {
+      logger.error(error)
+    }
   }
 
   async onMessage(callback: (message: IMessage) => void): Promise<void> {
-    await this.whatsappClient.onMessage((message) => {
-      const messageConverted = this.convertMessageVenomToMessage(message)
-      callback(messageConverted)
+    await this.whatsappClient.onMessage((message: Message) => {
+      const messageWithTelephone: any = {
+        ...message,
+        sender: {
+          ...message.sender,
+          telephone: this.idUsToTelephone(message.sender.id),
+        },
+      }
+      callback(messageWithTelephone as IMessage)
     })
   }
 
-  private convertMessageVenomToMessage(messageVenom: any) {
-    return messageVenom as IMessage
+  async sendText(to: string, content: string): Promise<object> {
+    return await this.whatsappClient.sendText(this.telephoneToIdUS(to), content)
+  }
+  async sendImage(to: string, content: string): Promise<object> {
+    return await this.whatsappClient.sendText(this.telephoneToIdUS(to), content)
+  }
+  async sendVoice(to: string, content: string): Promise<unknown> {
+    return await this.whatsappClient.sendVoice(this.telephoneToIdUS(to), content)
+  }
+  async sendSticker(to: string, path: string): Promise<false | SendStickerResult> {
+    return await this.whatsappClient.sendImageAsSticker(this.telephoneToIdUS(to), path)
+  }
+  async sendVideoAsGif(to: string, path: string, filename: string, caption: string): Promise<void> {
+    return await this.whatsappClient.sendVideoAsGif(this.telephoneToIdUS(to), path, filename, caption)
+  }
+  async sendDocument(to: string, path: string, filename?: string, caption?: string): Promise<unknown> {
+    return await this.whatsappClient.sendFile(this.telephoneToIdUS(to), path, filename, caption)
+  }
+
+  idUsToTelephone(idUS: string): string {
+    const telephone = idUS.replace(this.US, '')
+    return telephone
+  }
+  telephoneToIdUS(telephone: string): string {
+    const idUS = telephone.concat(this.US)
+    return idUS
   }
 }
 

d1f4fb221c7ca8f9956b2cbddc4bda867c0db9c8
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-25 20:00:26 -0300

    feat: removed  extends IWhatsappSender

--- src/adapters/interfaces/whatsappClient.ts
+++ src/adapters/interfaces/whatsappClient.ts
@@ -1,7 +1,5 @@
 import { IMessage } from '../../core/entities/Message'
-import { IWhatsappSender } from './whatsappSender'
-
-export interface IWhatsappClient extends IWhatsappSender {
+export interface IWhatsappClient {
   initialize(session: string): Promise<void>
   onMessage(callback: (message: IMessage) => void): Promise<void>
 }

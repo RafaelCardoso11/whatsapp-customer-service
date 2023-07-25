42845f5382bd19846b8affcbb89b3783eb376fe3
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-25 20:02:30 -0300

    fix: change to inject dependency and fixAll client to consultant

--- src/core/usecases/SendMessageToConsultantUseCase.spec.ts
+++ src/core/usecases/SendMessageToConsultantUseCase.spec.ts
@@ -5,52 +5,69 @@ import { SendMessageToConsultant } from './SendMessageToConsultantUseCase'
 import SenderClientMockAdapter from '../../adapters/SenderClientMockAdapter'
 import WhatsappClient from '../../infra/Whatsapp/Client'
 import { formatterMessageClientWithInfoClient } from '../../helpers/formatterMessageClientWithInfosClient'
+import { CommandsUseCase } from './commands'
+import { Consultant } from '../entities/Consultant'
 
 describe('SendMessageToConsultantUseCase', () => {
   it('should send message to Consultant', async () => {
     const clientMockAdapter = new SenderClientMockAdapter()
-    const whatsappClient = new WhatsappClient(clientMockAdapter)
+    const sender = new Sender(clientMockAdapter)
+    const commands = new CommandsUseCase(sender)
+    const clientWhatsapp = new WhatsappClient(clientMockAdapter, commands)
 
-    whatsappClient.initialize()
+    clientWhatsapp.initialize()
 
-    const sender = new Sender(whatsappClient)
     const sendMessageToConsultant = new SendMessageToConsultant(sender)
 
-    const client = new Client('1', 'Rafael', '9196320038')
+    const consultant: Consultant = {
+      _id: '1',
+      name: 'Rafael Cardoso',
+      telephone: '91996320038',
+    }
 
     const messageClient = 'Olá, boa tarde. Desejo comprar um produto.'
 
     const senderAdapterSpy = jest.spyOn(sender, 'sendText')
     const senderSpy = jest.spyOn(sender, 'sendText')
 
-    const messageSended = await sendMessageToConsultant.execute(EMessageType.TEXT, client.telephone, messageClient)
+    const messageSended = await sendMessageToConsultant.execute(EMessageType.TEXT, consultant.telephone, messageClient)
 
-    expect(senderAdapterSpy).toHaveBeenCalledWith(client.telephone, messageClient)
-    expect(senderSpy).toHaveBeenCalledWith(client.telephone, messageClient)
+    expect(senderAdapterSpy).toHaveBeenCalledWith(consultant.telephone, messageClient)
+    expect(senderSpy).toHaveBeenCalledWith(consultant.telephone, messageClient)
 
-    expect(messageSended).toEqual({ to: client.telephone, content: messageClient })
+    expect(messageSended).toEqual({ to: consultant.telephone, content: messageClient })
   })
   it('should send message text formatted to consultant with infos (name, nameSave, dateCurrent, content) consultant', async () => {
     const clientMockAdapter = new SenderClientMockAdapter()
-    const whatsappClient = new WhatsappClient(clientMockAdapter)
+    const sender = new Sender(clientMockAdapter)
+    const commands = new CommandsUseCase(sender)
+    const clientWhatsapp = new WhatsappClient(clientMockAdapter, commands)
 
-    whatsappClient.initialize()
-
-    const sender = new Sender(whatsappClient)
+    clientWhatsapp.initialize()
 
     const sendMessageToConsultant = new SendMessageToConsultant(sender)
 
     const client = new Client('1', 'Rafael', '9196320038')
     client.nameSave = 'Rafael Cliente'
 
+    const consultant: Consultant = {
+      _id: '1',
+      name: 'Rafael Cardoso',
+      telephone: '91996320038',
+    }
+
     const messageClient = 'Olá, tudo bem?'
 
-    const messageFormatted = await sendMessageToConsultant.messageFormattedWithInfosClient(client, messageClient)
+    const messageFormatted = await sendMessageToConsultant.messageFormattedWithInfosClient(
+      client,
+      messageClient,
+      consultant.telephone
+    )
 
     const messageClientFormatted = formatterMessageClientWithInfoClient(client, messageClient)
 
     expect(messageFormatted).toEqual({
-      to: client.telephone,
+      to: consultant.telephone,
       content: messageClientFormatted,
     })
   })

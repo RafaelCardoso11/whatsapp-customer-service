142c0239b8d33c039dc6f908f4e7d971fc992b77
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-26 15:31:44 -0300

    feat: constants errors

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,14 +1,12 @@
 const constants = {
-  error: {
-    NO_AVAILABLE_CONSULTANT: 'Nenhum consultor disponível',
-    NO_UPDATED_CLIENT: 'O Cliente não foi atualizado',
-    FAIL_TO_REGISTER_CLIENT_FOR_CONSULTANT: 'Erro ao cadastrar o cliente no Consultor.',
-    FAIL_TO_UPDATE_CLIENT_FOR_CONSULTANT: 'Erro ao fazer o Update do cliente Atual no Consultor.',
+  errors_to_whatsapp: {
+    commands: {
+      COMMAND_INVALID: `*Comando não reconhecido: {command}.* _Veja todos os comandos em #/comandos_`,
+      COMMAND_SUGGESTION: `_Você quis dizer *{commandSuggestion}*_?`,
+    },
     YOU_NOT_ARE_ATTENDIMENT: '_Você não está em um atendimento!_',
-    COMMAND_INVALID: `*Comando não reconhecido: {command}.* _Veja todos os comandos em #/comandos_`,
-    COMMAND_SUGGESTION: `_Você quis dizer *{commandSuggestion}*_?`,
   },
-  sucess: {
+  sucess_to_whatsapp: {
     NEW_CLIENT_FOR_CONSULTANT: '_Ei, Você tem um novo cliente para atender. O *{clientName}* está te esperando._',
     MESSAGE_WITH_NAME_CONSULTANT_AND_CONTENT: '*Consultor {consultantName}*\n{messageContent}',
     MESSAGE_WITH_INFO_CLIENT:
@@ -16,6 +14,21 @@ const constants = {
     MESSAGE_WAIT_FOR_CONSULTANT_1: `Olá, tudo bem? 😊\n` + `Em breve você será atendido por um de nossos consultores.`,
     MESSAGE_WAIT_FOR_CONSULTANT_2: `Para agilizar o nosso atendimento informe o seu nome e dúvida/pedido que já retornaremos.`,
   },
+  error: {
+    consultant: {
+      NO_AVAILABLE_CONSULTANT: 'Erro ao tentar encontrar um consultor disponível',
+      FAILED_TO_CREATE_CONSULTANT: 'Erro ao tentar criar um consultor',
+      FAILED_TO_UPDATED_CONSULTANT: 'Erro ao atualizaro o consultor',
+      FAILED_TO_REGISTER_CLIENT_FOR_CONSULTANT: 'Erro ao cadastrar o cliente no consultor',
+      CONSULTANT_NOT_FOUND: 'Consultor não encontrado',
+      FAILED_TO_FIND_BY_ID: 'Erro ao tentar encontrar um consultor por ID',
+      FAILED_TO_FIND_BY_ID_CLIENT: 'Erro ao tentar encontrar um consultor pelo ID do cliente',
+      FAILED_TO_FIND_BY_TELEPHONE: 'Erro ao tentar encontrar um consultor por TELEPHONE',
+      FAILED_TO_GET_ALL: 'Erro ao tentar buscar todos os consultores',
+      FAILED_TO_UPDATED_CLIENT_CURRENT: "Erro ao tentar atualizar o cliente atual do consultor"
+    },
+  },
+  sucess: {},
 }
 
 export default constants

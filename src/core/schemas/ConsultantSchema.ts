5572d782a07bcf8a3d977950af7e80b9c50017d5
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-25 23:32:56 -0300

    fix: number to telephone prop and required clientCurrent

--- src/core/schemas/ConsultantSchema.ts
+++ src/core/schemas/ConsultantSchema.ts
@@ -6,14 +6,15 @@ const ConsultantSchema = Odm.createSchema({
     type: 'string',
     required: true,
   },
-  number: {
+  telephone: {
     type: 'string',
     required: true,
   },
   clientCurrent: {
-    _id: { type: Odm.Schema.Types.ObjectId, auto: false },
-    name: { type: 'string', required: false },
-    number: { type: 'string', required: false },
+    _id: { type: Odm.Schema.Types.ObjectId, auto: true },
+    name: { type: 'string', required: true },
+    nameSave: { type: 'string', required: true },
+    telephone: { type: 'string', required: true },
   },
 })
 export const ConsultantModel = Odm.createModel('Consultants', ConsultantSchema)

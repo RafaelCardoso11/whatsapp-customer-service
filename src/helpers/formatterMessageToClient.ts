7fa0b1e2547a52c94d036542b771d7885da82fb7
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-26 20:31:56 -0300

    feat: formatterMessageToClient

---
+++ src/helpers/formatterMessageToClient.ts
@@ -0,0 +1,11 @@
+import constants from '../constants'
+
+function formatterMessageToClient(consultantName: string, messageContent: string): string {
+  const { MESSAGE_WITH_NAME_CONSULTANT_AND_CONTENT } = constants.sucess_to_whatsapp
+  return MESSAGE_WITH_NAME_CONSULTANT_AND_CONTENT.replace('{consultantName}', consultantName).replace(
+    '{messageContent}',
+    messageContent
+  )
+}
+
+export { formatterMessageToClient }

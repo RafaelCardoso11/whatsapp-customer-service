fe101759e37f756f373ef81722c4fa3fcf61ad07
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-26 22:53:11 -0300

    feat: created interface to dependencies to client

---
+++ src/infra/Whatsapp/ClientDependencies.ts
@@ -0,0 +1,15 @@
+import { IWhatsappClient } from "../../adapters/interfaces/whatsappClient";
+import { CommandsUseCase } from "../../core/usecases/Commands";
+import { ConsultantUseCase } from "../../core/usecases/Consultant";
+import { QueueAttendimentUseCase } from "../../core/usecases/QueueAttendiment";
+import { SenderUseCase } from "../../core/usecases/Sender";
+
+export class WhatsappClientDependencies {
+  constructor(
+    public client: IWhatsappClient,
+    public queueAttendimentUseCase: QueueAttendimentUseCase,
+    public commandsUseCase: CommandsUseCase,
+    public consultantUseCase: ConsultantUseCase,
+    public senderUseCase: SenderUseCase
+  ) {}
+}

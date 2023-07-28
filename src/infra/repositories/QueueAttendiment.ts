af7a7abebf99b21587abc1254a76576d1a4de3dd
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-27 22:51:25 -0300

    feat: add client and data props in created

--- src/infra/repositories/QueueAttendiment.ts
+++ src/infra/repositories/QueueAttendiment.ts
@@ -3,19 +3,21 @@ import { QueueAttendiment } from '../../core/entities/QueueAttendiment';
 import { QueueAttendimentModel } from '../../core/schemas/QueueAttendiment';
 
 class QueueAttendimentRepository {
-  async add(queueAttendiment: QueueAttendiment): Promise<QueueAttendiment> {
+  async add({ client, date }: QueueAttendiment): Promise<QueueAttendiment> {
     const lastAttendiment = await QueueAttendimentModel.findOne().sort({ _id: -1 }).exec();
 
     const numberIncremented = lastAttendiment ? lastAttendiment.number + 1 : 1;
+
     const created = (
       await QueueAttendimentModel.create({
-        ...queueAttendiment,
+        client,
+        date,
         number: numberIncremented,
       })
     ).toJSON() as QueueAttendiment;
 
     logger.info('Created attendiment in queue. Number #', numberIncremented);
-    return { ...created, number: numberIncremented };
+    return created;
   }
   async remove(idAttendiment: string): Promise<QueueAttendiment> {
     const removed = (await QueueAttendimentModel.findByIdAndRemove(idAttendiment)).toJSON() as QueueAttendiment;

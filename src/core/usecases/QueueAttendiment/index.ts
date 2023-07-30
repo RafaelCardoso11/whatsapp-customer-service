de1a1420e68a64d0b4137677d10558f62bd8e201
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-30 12:27:45 -0300

    feat: add removeQueueAttendimentAndRemoveClientCurrent and rename queueAttendimentRepository

--- src/core/usecases/QueueAttendiment/index.ts
+++ src/core/usecases/QueueAttendiment/index.ts
@@ -1,25 +1,47 @@
+import { logger } from '../../../infra/logger/logger';
+import { ConsultantRepository } from '../../../infra/repositories/Consultant';
 import { QueueAttendimentRepository } from '../../../infra/repositories/QueueAttendiment';
 import { Client } from '../../entities/Client';
 import { QueueAttendiment as QueueAttendimentEntity } from '../../entities/QueueAttendiment';
 
 export class QueueAttendimentUseCase {
-  constructor(private readonly queueAttendiment: QueueAttendimentRepository) {}
+  constructor(
+    private readonly queueAttendimentRepository: QueueAttendimentRepository,
+    private readonly consultantRepository: ConsultantRepository
+  ) {}
 
   public async add(client: Client, message: string): Promise<QueueAttendimentEntity> {
-    return await this.queueAttendiment.add({ client, message: { content: message } });
+    return await this.queueAttendimentRepository.add({ client, message: { content: message } });
   }
 
-  public async remove(idAttendiment: string): Promise<QueueAttendimentEntity> {
-    return await this.queueAttendiment.remove(idAttendiment);
+  public async getTheFistInQueue(): Promise<QueueAttendimentEntity> {
+    return await this.queueAttendimentRepository.getTheFistInQueue();
+  }
+  public async removeById(idAttendiment: string): Promise<QueueAttendimentEntity> {
+    return await this.queueAttendimentRepository.removeById(idAttendiment);
   }
   public async clientInQueueAttendiment(telephoneClient: string): Promise<QueueAttendimentEntity> {
-    return await this.queueAttendiment.getByTelephone(telephoneClient);
+    return await this.queueAttendimentRepository.getByTelephone(telephoneClient);
   }
 
   public async saveMessageInAttendiment(
     telephoneClient: string,
     contentMessage: string
   ): Promise<QueueAttendimentEntity> {
-    return await this.queueAttendiment.updateMessages(telephoneClient, contentMessage);
+    return await this.queueAttendimentRepository.updateMessages(telephoneClient, contentMessage);
+  }
+
+  public async removeQueueAttendimentAndRemoveClientCurrent(idConsultant: string): Promise<void> {
+    try {
+      const theFirstInQueue = await this.queueAttendimentRepository.getTheFistInQueue();
+
+      if (theFirstInQueue) {
+        await this.queueAttendimentRepository.removeById(theFirstInQueue._id);
+
+        await this.consultantRepository.removeClientCurrentPropertyFromConsultant(idConsultant);
+      }
+    } catch (error) {
+      logger.info('Nenhum cliente na fila de atendimento');
+    }
   }
 }

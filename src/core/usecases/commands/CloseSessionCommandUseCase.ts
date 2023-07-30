23e00c47b9f599518bfa80c05e24c202cde7b93b
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-07-30 11:38:03 -0300

    feat: command close session

--- src/core/usecases/commands/CloseSessionCommandUseCase.ts
+++ src/core/usecases/commands/CloseSessionCommandUseCase.ts
@@ -1,42 +1,58 @@
+import { convertMinutesToMilliseconds } from '../../../helpers/minuteToMiliseconds';
+import { AttendimentRepository } from '../../../infra/repositories/Attendiment';
+import { ConsultantRepository } from '../../../infra/repositories/Consultant';
 import { Sender } from '../../../infra/whatsapp/Sender';
+import { Client } from '../../entities/Client';
 import { Consultant } from '../../entities/Consultant';
-import { ICommand } from './interfaces/command';
+import { WaitMessage } from './WaitMessage';
 
-export class CloseSessionCommand implements ICommand {
-  constructor(private readonly sender: Sender) {}
+export class CloseSessionCommand extends WaitMessage {
+  constructor(
+    private readonly consultantRepository: ConsultantRepository,
+    private readonly attendimentRepository: AttendimentRepository,
+    protected readonly sender: Sender
+  ) {
+    super(sender);
+  }
+  async execute(consultant: Consultant): Promise<boolean> {
+    if (consultant.clientCurrent) {
+      const sendedMessageClosseSession = await this.sender.sendText(
+        consultant.telephone,
+        '*ATENÇÃO:* _Você realmente deseja encerrar o atendimento?_ \n Apenas (*SIM* ou *NÃO*)'
+      );
+      if (sendedMessageClosseSession) {
+        const MAX_TIME_OUT = convertMinutesToMilliseconds(10);
+        const ResolvedValue = 'SIM';
+        const RejectedValue = 'NÃO';
+        const response = await this.waitForResponseFromConsultant(consultant.telephone, MAX_TIME_OUT, RejectedValue);
 
-  async execute(consultant: Consultant, contentNextMessageClient: () => Promise<string>): Promise<void> {
-    this.sender.sendText(
-      consultant.telephone,
-      '*ATENÇÃO:* _Você realmente deseja encerrar o atendimento?_ \n Apenas (*SIM* ou *NÃO*)'
-    );
+        if (response.toUpperCase().includes(ResolvedValue)) {
+          const consultantAfterCloseAttendiment = await this.consultantRepository.updateClientCurrent(
+            consultant._id,
+            {} as Client
+          );
 
-    const message = await contentNextMessageClient();
-    if (message) {
-      // if (clientCurrent?._id) {
-      //   await consultantRepository.updateClientCurrent(_id, clientCurrentEmpty)
-      //   const attendimentCreated = await attendimentRepository.create({
-      //     attendimentStars: 5,
-      //     avaliation: [
-      //       {
-      //         content: 'OK',
-      //       },
-      //     ],
-      //     consultant: consultant,
-      //     date: new Date(),
-      //     client: clientCurrent,
-      //   })
-      //   if (attendimentCreated?.number) {
-      //     this.sender.sendText(
-      //       consultant.telephone,
-      //       `_Atendimento *#${attendimentCreated.number}* Encerrado com Sucesso_`
-      //     )
-      //   }
-      //   logger.error(
-      //     `
-      // Erro ao encerrar o Atendimento. Erro:` + JSON.stringify(attendimentCreated)
-      //   )
+          if (consultantAfterCloseAttendiment) {
+            await this.sender.sendText(consultant.telephone, '_Atendimento encerrado!_');
+            await this.attendimentRepository.create({
+              attendimentStars: 0,
+              avaliation: [],
+              consultant: consultantAfterCloseAttendiment,
+              client: consultant.clientCurrent,
+              date: new Date(),
+            });
+            return true;
+          }
+        }
+      } else {
+        await this.sender.sendText(
+          consultant.telephone,
+          `_O atendimento com o cliente ${consultant.clientCurrent?.name} não foi encerrado!_`
+        );
+      }
+    } else {
+      await this.sender.sendText(consultant.telephone, 'Você não está em um atendimento para encerra-lo!');
     }
-    this.sender.sendText(consultant.telephone, 'Você não está em um atendimento!.');
+    return false;
   }
 }

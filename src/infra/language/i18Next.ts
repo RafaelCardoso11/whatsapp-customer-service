17160f5296a12dc24928bc3be33336ec1026d5e4
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-01 22:45:29 -0300

    chore: i18next init implemetation infraestructure.

---
+++ src/infra/language/i18Next.ts
@@ -0,0 +1,13 @@
+import i18next from 'i18next';
+
+i18next.init({
+  lng: 'ptBR',
+  debug: true,
+  resources: {
+    ptBR: {
+      translation: {
+        key: 'hello world',
+      },
+    },
+  },
+});

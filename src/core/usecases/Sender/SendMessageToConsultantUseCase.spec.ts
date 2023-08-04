9945edb6501e6ef5f2a3ad628cef4a63b62b0480
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-08-04 20:28:57 -0300

    test: use translate locale

--- src/core/usecases/Sender/SendMessageToConsultantUseCase.spec.ts
+++ src/core/usecases/Sender/SendMessageToConsultantUseCase.spec.ts
@@ -3,9 +3,9 @@ import { Client } from '../../entities/Client';
 import { Consultant } from '../../entities/Consultant';
 
 import { SenderUseCase } from '.';
-import { formatterMessageToConsultant } from '../../../helpers/formatterMessageToConsultant';
 
 import { WhatsappClientMemoryFactory } from '../../../infra/whatsapp/ClientMemoryFactory';
+import { LanguageManagerSingleton } from '../../../infra/language';
 
 describe('SendMessageToConsultantUseCase', () => {
   let senderUseCase: SenderUseCase;
@@ -34,12 +34,15 @@ describe('SendMessageToConsultantUseCase', () => {
       messageClient,
       consultant.telephone
     );
-
-    const messageClientFormatted = formatterMessageToConsultant(client, messageClient);
-
+    const dateCurrent = new Date();
     expect(messageFormatted).toEqual({
       to: consultant.telephone,
-      content: messageClientFormatted,
+      content: LanguageManagerSingleton.translate('attendiment:ATTENDIMENT_MESSAGE_WITH_INFO_CLIENT', {
+        nameSaveClient: client.nameSave,
+        nameClient: client.name,
+        dateCurrent,
+        messageContent: messageClient,
+      }),
     });
   });
 });
